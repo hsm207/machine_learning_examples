d87375233c43706c963802d1d438d0b2f0906e78
hsm207 <hsm207@users.noreply.github.com>
2020-08-22 18:31:27 +0000

    My solution to optimistic.py

--- rl/optimistic.py
+++ rl/optimistic.py
@@ -3,69 +3,86 @@
 # https://www.udemy.com/bayesian-machine-learning-in-python-ab-testing
 from __future__ import print_function, division
 from builtins import range
+
 # Note: you may need to update your version of future
 # sudo pip install -U future
 
 
 import matplotlib.pyplot as plt
 import numpy as np
-
-
-NUM_TRIALS = 10000
-EPS = 0.1
-BANDIT_PROBABILITIES = [0.2, 0.5, 0.75]
+from typing import List
 
 
 class Bandit:
-  def __init__(self, p):
-    # p: the win rate
-    self.p = p
-    self.p_estimate = 5.
-    self.N = 1. # num samples collected so far
+    def __init__(self, p):
+        # p: the win rate
+        self.p = p
+        self.p_estimate = 5.0
+        self.N = 1.0  # num samples collected so far
 
-  def pull(self):
-    # draw a 1 with probability p
-    return np.random.random() < self.p
+    def pull(self):
+        # draw a 1 with probability p
+        return np.random.random() < self.p
 
-  def update(self, x):
-    self.N += 1.
-    self.p_estimate = ((self.N - 1)*self.p_estimate + x) / self.N
-
-
-def experiment():
-  bandits = [Bandit(p) for p in BANDIT_PROBABILITIES]
-
-  rewards = np.zeros(NUM_TRIALS)
-  for i in range(NUM_TRIALS):
-    # use optimistic initial values to select the next bandit
-    j = np.argmax([b.p_estimate for b in bandits])
-
-    # pull the arm for the bandit with the largest sample
-    x = bandits[j].pull()
-
-    # update rewards log
-    rewards[i] = x
-
-    # update the distribution for the bandit whose arm we just pulled
-    bandits[j].update(x)
+    def update(self, x):
+        self.N += 1.0
+        self.p_estimate = ((self.N - 1) * self.p_estimate + x) / self.N
 
 
-  # print mean estimates for each bandit
-  for b in bandits:
-    print("mean estimate:", b.p_estimate)
+def experiment(
+    num_trials: int = 10000,
+    eps: float = 0.1,
+    bandit_probabilities: List[float] = [0.2, 0.5, 0.75],
+):
+    bandits = [Bandit(p) for p in bandit_probabilities]
+    optimal_bandit = np.argmax([b.p for b in bandits])
 
-  # print total reward
-  print("total reward earned:", rewards.sum())
-  print("overall win rate:", rewards.sum() / NUM_TRIALS)
-  print("num times selected each bandit:", [b.N for b in bandits])
+    print(f"Given bandits with rewards:")
+    for i, b in enumerate(bandits, 1):
+        print(f"  bandit {i}: {b.p:.2f}")
+    print(
+        f"Optimal bandit is {optimal_bandit + 1} with reward {bandits[optimal_bandit].p}"
+    )
 
-  # plot the results
-  cumulative_rewards = np.cumsum(rewards)
-  win_rates = cumulative_rewards / (np.arange(NUM_TRIALS) + 1)
-  plt.ylim([0, 1])
-  plt.plot(win_rates)
-  plt.plot(np.ones(NUM_TRIALS)*np.max(BANDIT_PROBABILITIES))
-  plt.show()
+    rewards = np.zeros(num_trials)
+    preferred_bandit = 0
+    print(f"Starting with bandit {preferred_bandit + 1} ...")
+
+    for i in range(num_trials):
+        # use optimistic initial values to select the next bandit
+        j = np.argmax([b.p_estimate for b in bandits])
+        if j != preferred_bandit:
+            preferred_bandit = j
+            print(
+                f"Switching to bandit {preferred_bandit + 1} at iteration {i + 1}. Estimated rewards: {[b.p_estimate for b in bandits]}"
+            )
+
+        # pull the arm for the bandit with the largest sample
+        x = bandits[j].pull()
+
+        # update rewards log
+        rewards[i] = x
+
+        # update the distribution for the bandit whose arm we just pulled
+        bandits[j].update(x)
+
+    # print mean estimates for each bandit
+    for i, b in enumerate(bandits, 1):
+        print(f"mean estimate of bandit {i}: {b.p_estimate:.4f}")
+
+    # print total reward
+    print("total reward earned:", rewards.sum())
+    print("overall win rate:", rewards.sum() / num_trials)
+    print("num times selected each bandit:", [b.N for b in bandits])
+
+    # plot the results
+    cumulative_rewards = np.cumsum(rewards)
+    win_rates = cumulative_rewards / (np.arange(num_trials) + 1)
+    plt.ylim([0, 1])
+    plt.plot(win_rates)
+    plt.plot(np.ones(num_trials) * np.max(bandit_probabilities))
+    plt.show()
+
 
 if __name__ == "__main__":
-  experiment()
+    experiment()
